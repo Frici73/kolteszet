60b24d3dc3b8514d82c65ade25dda67f62280322
Frici73 <friedmann.mate0329@gmail.com>
2026-05-11 17:45:13 +0200

    name and crash fix

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,6 @@ const config: CapacitorConfig = {
   appId: 'hu.shadowarts.app',
   appName: 'ShadowArts',
   webDir: 'dist',
-  bundledWebRuntime: false,
   server: {
     androidScheme: 'https',
   },
